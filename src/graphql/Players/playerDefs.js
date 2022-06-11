c677e04cf6fcd08cfdecbdf9f4aa8e767e368502
Chacaponquin <hectorangel2001@gmail.com>
2022-06-11 17:09:55 -0400

    Buneas

--- src/graphql/Players/playerDefs.js
+++ src/graphql/Players/playerDefs.js
@@ -64,7 +64,7 @@ export const playerSchema = gql `
   type Query {
     findFreePlayers: [Player]!
     fetchOwnPlayers(team: FetchOwnPlayersInput!): [Player]!
-    fetchAllPlayers: [Player]!
+    fetchAllPlayers: [Player]
   }
 
   type Mutation {
